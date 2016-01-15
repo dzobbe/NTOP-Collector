8789d2ad4c875e51bd61b9dfa0a4cf9a6d3ecebe
dzobbe <mazzeo.gio@gmail.com>
2016-01-15 11:52:47 +0100

    Create EntryActiveSessions.java
    Class to represent an active session entry

---
+++ EntryActiveSessions.java
@@ -0,0 +1,44 @@
+package antiDoS.sentinel;
+
+public class EntryActiveSession {
+
+	private String client;
+	private String server;
+	private Float dataSent;
+	private Float dataRcv;
+	private Long timeOfCreation_ms;
+	
+
+	public String getClient() {
+		return client;
+	}
+	public String getServer() {
+		return server;
+	}
+	public Float getDataSent() {
+		return dataSent;
+	}
+	public Float getDataRcv() {
+		return dataRcv;
+	}
+	public Long getTimeOfCreation() {
+		return timeOfCreation_ms;
+	}
+
+	public void setClient(String client) {
+		this.client = client;
+	}
+	public void setServer(String server) {
+		this.server = server;
+	}
+	public void setDataSent(Float dataSent) {
+		this.dataSent = dataSent;
+	}
+	public void setDataRcv(Float dataRcv) {
+		this.dataRcv = dataRcv;
+	}
+	public void setTimeOfCreation(Long timeOfCreation) {
+		this.timeOfCreation_ms = timeOfCreation;
+	}
+	
+}
